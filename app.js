d8369e4ca8d68a132f4261203c7b0ad7f5202437
quangloc <locpq0311@gmail.com>
2018-11-12 22:35:57 +0700

    do authentication

--- app.js
+++ app.js
@@ -5,7 +5,38 @@ var express     = require("express"),
     flash       = require("connect-flash"),
     passport    = require("passport"),
     LocalStrategy = require("passport-local"),
+    Photo       = require("./models/photo"),
+    Comment     = require("./models/comment")
+    User      	= require("./models/user"),
     methodOverride = require("method-override");
+
+
+mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+	secret: "",
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
+
+
+
 
 // Routes
 var indexRouter = require("./routes/index");
@@ -15,10 +46,3 @@ var commentRouter = require("./routes/comments");
 var likeRouter = require("./routes/likes");
 var followRouter = require("./routes/follows");
 var exploreRouter = require("./routes/explore");
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
-app.use(methodOverride("_method"));
-app.use(flash());
-
