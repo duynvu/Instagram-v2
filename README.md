52d214090b7a996cec1e03b7ad833b439867e635
quangloc <locpq0311@gmail.com>
2018-11-13 22:07:21 +0700

    done logic authentication

---
+++ README.md
@@ -0,0 +1,8 @@
+//use mongoose
+
+install mongoose > make directory to mongoose db folder (eg: cd C:\Program Files\MongoDB\Server\4.0\bin) > mongo
+
+1. show dbs
+2. use instagram-v2
+3. show collections
+4. db.users.find()
--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 var express     = require("express"),
     app         = express();
-//     bodyParser  = require("body-parser"),
-//     mongoose    = require("mongoose"),
-//     flash       = require("connect-flash"),
-//     passport    = require("passport"),
-//     LocalStrategy = require("passport-local"),
-//     Photo       = require("./models/photo"),
-//     Comment     = require("./models/comment")
-//     User      	= require("./models/user"),
-//     methodOverride = require("method-override");
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    flash       = require("connect-flash"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    // Photo       = require("./models/photo"),
+    // Comment     = require("./models/comment")
+    User      	= require("./models/user"),
+    methodOverride = require("method-override");
 
 
-<<<<<<< HEAD
 mongoose.connect("mongodb://localhost/instagram-v2", { useNewUrlParser: true });
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -25,38 +25,24 @@ app.use(require("express-session")({
 	resave: false,
 	saveUninitialized: false
 }));
-=======
-// mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.set("view engine", "ejs");
-// app.use(express.static(__dirname + "/public"));
-// app.use(methodOverride("_method"));
-// app.use(flash());
 
-// // PASSPORT CONFIGURATION
-// app.use(require("express-session")({
-// 	secret: "",
-// 	resave: false,
-// 	saveUninitialized: false
-// }));
->>>>>>> 6357470d471da454fda42055d57f9330b4cde784
 
-// app.use(passport.initialize());
-// app.use(passport.session());
-// passport.use(new LocalStrategy(User.authenticate()));
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', function(req, res) {
     res.send("ok");
 })
 
-// app.use(function(req, res, next){
-//     res.locals.currentUser = req.user;
-//     res.locals.error   = req.flash("error");
-//     res.locals.success  = req.flash("success");
-//     next();
-// });
+app.use(function(req, res, next){
+    //res.locals.currentUser = req.user;
+    res.locals.error   = req.flash("error");
+    res.locals.success  = req.flash("success");
+    next();
+});
 
 app.get("/photos",function(req,res){
     res.render("index")
@@ -68,21 +54,20 @@ app.get("/photos",function(req,res){
 
 // show register form
 app.get("/register",function(req,res){
-    res.render("register");
+    res.render("registration");
 });
 //handle sign up logic
-app.post("/register",function(req,res){
-    var newUser = newUser({username: req.body.username});
-    User.register(newUser,req.body.password, function(err,user){
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
         if(err){
             req.flash("error", err.message);
-            return res.render("register")
-        } else {
-            req.flash("success","Welcome to Instagram" + user.username);
-            passport.authenticate("local")(req,res,function(){
-                res.redirect("/photos");
-            });
+            return res.render("registration");
         }
+        passport.authenticate("local")(req, res, function(){
+           req.flash("success", "Welcome to Instagram" + user.username);
+           res.redirect("/photos"); 
+        });
     });
 });
 
@@ -98,7 +83,7 @@ app.post("/login", passport.authenticate("local",
     }), function(req,res){
 });
 
-//logic route
+//logout route
 // app.get("/logout",function(req,res){
 //     req.logout();
 //     req.flash("success","Logged you out!");
