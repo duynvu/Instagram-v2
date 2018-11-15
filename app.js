99f642e6b2207dce9d272c2bc2c1c2a42ed19e45
quangloc <locpq0311@gmail.com>
2018-11-15 15:25:30 +0700

    refactor authentication

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ var express     = require("express"),
     flash       = require("connect-flash"),
     passport    = require("passport"),
     LocalStrategy = require("passport-local"),
-    // Photo       = require("./models/photo"),
+    Photo       = require("./models/photo"),
     // Comment     = require("./models/comment")
-    User      	= require("./models/user"),
+    User        = require("./models/user"),
     methodOverride = require("method-override");
+
+//requring routes
+var indexRoutes      = require("./routes/index");
+
 
 
 mongoose.connect("mongodb://localhost/instagram-v2", { useNewUrlParser: true });
@@ -21,9 +25,9 @@ app.use(flash());
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
-	secret: "Authenticate is importane",
-	resave: false,
-	saveUninitialized: false
+    secret: "Authenticate is importane",
+    resave: false,
+    saveUninitialized: false
 }));
 
 
@@ -33,9 +37,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.get('/', function(req, res) {
-    res.send("ok");
-})
+
 
 app.use(function(req, res, next){
     //res.locals.currentUser = req.user;
@@ -49,54 +51,7 @@ app.get("/photos",function(req,res){
 })
 
 
-
-//AUTH ROUTES
-
-// show register form
-app.get("/register",function(req,res){
-    res.render("registration");
-});
-//handle sign up logic
-app.post("/register", function(req, res){
-    var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function(err, user){
-        if(err){
-            req.flash("error", err.message);
-            return res.render("registration");
-        }
-        passport.authenticate("local")(req, res, function(){
-           req.flash("success", "Welcome to Instagram" + user.username);
-           res.redirect("/photos"); 
-        });
-    });
-});
-
-//show login form
-app.get("/login",function(req,res){
-    res.render("login");
-});
-// handling login logic
-app.post("/login", passport.authenticate("local",
-    {
-        successRedirect: "/photos",
-        failureRedirect: "/login"
-    }), function(req,res){
-});
-
-//logout route
-// app.get("/logout",function(req,res){
-//     req.logout();
-//     req.flash("success","Logged you out!");
-//     res.redirect("/photos");
-// })
-
-//middleware
-// function isLoggedIn(req, res, next){
-//     if(req.isAuthenticated()){
-//         return next();
-//     }
-//     res.redirect("/login");
-// }
+app.use("/", indexRoutes);
 
 app.listen(3000, () => {
     console.log("Server is listening");
