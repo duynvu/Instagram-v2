ba3a6348ed9c9804a813e74f4c7f603bfd8985ee
Vu Duy <duynvu@gmail.com>
2018-12-19 11:13:44 +0700

    fix port

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.use("/users",userRoutes);
 app.use("/photos/:id/comments", commentRoutes);
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is listening");
 })
